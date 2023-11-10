516aa274622e920c33d5ed78d8116a72828473f5
Tatyana0808 <tas0808@gmail.com>
2023-11-10 16:00:55 +0300

    создала логин и пароль

--- loginPage.js
+++ loginPage.js
@@ -1,2 +1,16 @@
-const buttonElement = document.getElementById('login-button')
+import { login } from ".api.js"
 
+const buttonElement = document.getElementById("login-button")
+const loginInputElement = document.getElementById("login-input")
+const passwordInputElement = document.getElementById("password-input")
+
+buttonElement.addEventListener("click", () => {
+login({
+    login: loginInputElement.value,
+    password: passwordInputElement.value,
+}
+    
+).then((responseData) => {
+console.log(responseData)
+    })
+})
